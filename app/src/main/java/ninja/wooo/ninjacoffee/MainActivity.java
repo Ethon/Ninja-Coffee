241b0795a7901a53f67499072ca5e982ee6f30c4
holyhead <kingfatbarsch@yahoo.de>
2015-05-14 01:02:29 +0200

    #2: Added last change text field

--- app/src/main/java/ninja/wooo/ninjacoffee/MainActivity.java
+++ app/src/main/java/ninja/wooo/ninjacoffee/MainActivity.java
@@ -10,12 +10,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public static final String PREF_LAST_COFFEE_CHANGE = "lastCoffeeChange";
+    public static final String PREF_COFFEE_COUNT = "coffeeCount";
+
     private SharedPreferences sharedPreferences;
     private EditText editText;
+    private TextView lastChangeText;
+    // TODO: I've tried DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT) however this will always display the AM/PM in my emulator...
+    // So for now I simply formatted it this way:
+    private DateFormat lastChangeFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +36,15 @@ public class MainActivity extends ActionBarActivity {
 
         sharedPreferences = this.getSharedPreferences(getString(R.string.coffee_credit), Context.MODE_PRIVATE);
         editText = (EditText) findViewById(R.id.coffeeCreditField);
+        lastChangeText = (TextView) findViewById(R.id.lastChangeField);
 
-        editText.setText(sharedPreferences.getString("coffeeCount", "0"));
+
+        editText.setText(sharedPreferences.getString(PREF_COFFEE_COUNT, "0"));
+
+        Long lastCoffeeChange = sharedPreferences.getLong(PREF_LAST_COFFEE_CHANGE, 0L);
+        if (lastCoffeeChange != 0L) {
+            lastChangeText.setText(lastChangeFormat.format(new Date(lastCoffeeChange)));
+        }
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -39,12 +58,15 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                if(!s.toString().trim().isEmpty()) {
-                    editor.putString("coffeeCount", s.toString());
+                if (!s.toString().trim().isEmpty()) {
+                    editor.putString(PREF_COFFEE_COUNT, s.toString());
                 } else {
-                    editor.putString("coffeeCount", "0");
+                    editor.putString(PREF_COFFEE_COUNT, "0");
                     editText.setText("0");
                 }
+                Date now = new Date();
+                editor.putLong(PREF_LAST_COFFEE_CHANGE, now.getTime());
+                lastChangeText.setText(lastChangeFormat.format(now));
                 editor.commit();
             }
         });
